49a7cf8cc1a7be34eedf76e5402bfee3155003eb
Yue Wu <ywu48@lsu.edu>
2018-02-26 12:34:46 -0600

    Summary

---
+++ js/code-assignment-03.js
@@ -0,0 +1,12 @@
+let myNumber = 7
+let myString = 'Some text'
+let myBoolean = true
+let myArray = ['apple', 'orange', 'pear']
+console.log(myNumber)
+console.log(myString)
+console.log(myBoolean)
+console.log(myArray)
+console.log('Project ID: ' + projectId)
+console.log('Title: ' + projectTitle)
+console.log('Does it have a thumbnail image? ' + hasThumbnail)
+console.log('Keywords: ' + projectKeywords)
