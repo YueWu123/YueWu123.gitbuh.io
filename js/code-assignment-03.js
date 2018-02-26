08c36906cfb9610d7fdb093e1e762594f93e6e54
Yue Wu <ywu48@lsu.edu>
2018-02-26 13:37:07 -0600

    Summary

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,12 +1,12 @@
 // assign values to a set of variables representing a project's metadata
-let projectId = yuewu123
-let projectTitle = 'GEOG 4046 Assignment Portfolio'
-let projectUrl = 'https://yuewu123.github.io/YueWu123.gitbuh.io/'
-let projectThumb= true
-let projectname = ['book assignment1', 'book assignment2', 'book assignment3', 'book assignment5']
+let WebId = yuewu123
+let WebTitle = 'GEOG 4046 Assignment Portfolio'
+let WebtUrl = 'https://yuewu123.github.io/YueWu123.gitbuh.io/'
+let WebThumb= true
+let Assignmentname = ['book assignment1', 'book assignment2', 'book assignment3', 'book assignment5']
 // display messages in the browser's developer console, combining the values with text
-console.log('ID: ' + projectId)
-console.log('Title: ' + projectTitle)
-console.log('has thumbnail?' + projectThumb)
-console.log('URL' + projectUrl)
-console.log('Names: ' + projectname)
+console.log('ID: ' + WebId)
+console.log('Title: ' + WebTitle)
+console.log('has thumbnail?' + WebThumb)
+console.log('URL' + WebtUrl)
+console.log('Names: ' + Assignmentname)
