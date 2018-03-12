763466b3a22cb833eb5370cb4c0d32628f98b90b
Yue Wu <ywu48@lsu.edu>
2018-03-12 09:53:30 -0500

    Summary

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,12 +1,13 @@
 // assign values to a set of variables representing a project's metadata
-let WebId = 'yuewu123'
-let WebTitle = 'GEOG 4046 Assignment Portfolio'
-let WebtUrl = 'https://yuewu123.github.io/YueWu123.gitbuh.io/'
-let WebThumb= true
-let Assignmentname = ['book assignment1', 'book assignment2', 'book assignment3', 'book assignment5']
+let webId = '1'
+let webTitle = 'GEOG 4046 Assignment Portfolio'
+let webtUrl = 'https://yuewu123.github.io/YueWu123.gitbuh.io/'
+let webThumb= true
+
+
 // display messages in the browser's developer console, combining the values with text
-console.log('ID: ' + WebId)
-console.log('Title: ' + WebTitle)
+console.log('ID:' + WebId)
+console.log('Title:' + WebTitle)
 console.log('has thumbnail?' + WebThumb)
 console.log('URL' + WebtUrl)
 console.log('Names: ' + Assignmentname)
---
+++ js/code-assignment-04.js
@@ -0,0 +1,48 @@
+let webId = '1'
+let webTitle = 'GEOG 4046 Assignment Portfolio'
+let webtUrl = 'https://yuewu123.github.io/YueWu123.gitbuh.io/'
+let projectDesc = 'This is a collection of maps created for Geog4046: web GIS'
+let webThumb= true
+
+let webArray = [
+  1,
+  'Geog4046 Assignment Portfolio',
+  'https://yuewu123.github.io/YueWu123.gitbuh.io/',
+  'This is a collection of maps created for Geog4046: web GIS',
+  true
+]
+
+let webObject = {
+  id: 1,
+  title: 'GEOG 4046 Assignment Portfolio',
+  url: 'https://yuewu123.github.io/YueWu123.gitbuh.io/',
+  desc: 'This is a collection of maps created for Geog4046: web GIS',
+  thumb: true,
+}
+
+let projectId = '2'
+let projectTitle = 'Book assignment1'
+let projectUrl = 'https://www.arcgis.com/apps/MapJournal/index.html?appid=6bf86f29b2ce42399e681e2c9832d429'
+let projtectDesc = 'A bike trip in San Franscisco'
+let projectThumb = true
+
+let projectArray = [
+  2,
+  'Book assignment1',
+  'https://www.arcgis.com/apps/MapJournal/index.html?appid=6bf86f29b2ce42399e681e2c9832d429',
+  'A bike trip in San Franscisco',
+  true
+]
+
+let projectObject = {
+  id: 2,
+  title: 'Book assignment1',
+  url: 'https://www.arcgis.com/apps/MapJournal/index.html?appid=6bf86f29b2ce42399e681e2c9832d429',
+  desc: 'A bike trip in San Franscisco',
+  thumb: true,
+}
+
+
+console.log(projectArray[2])
+console.log(projectObject.title)
+console.log(projectObject['Book assignment1'])
